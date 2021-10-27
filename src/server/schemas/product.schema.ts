17ce6787ec0f7afba39ce6ab8edf0998b58299ab
mekdes45 <mekmis2020@gmail.com>
2021-10-27 13:23:14 -0700

    feat(schema,model):add product sche and model

--- src/server/schemas/product.schema.ts
+++ src/server/schemas/product.schema.ts
@@ -1,14 +1,16 @@
 
 import mongoose from 'mongoose';
-import { Product } from './../../shared/models/product.model';
+import { Product } from '../../shared/models/product.model.js';
 const {Schema, model} = mongoose
 
 const productSchema = new Schema<Product>({
+    _id: {type: String, required: true},
     title: {type: String, required: true},
     price: {type: Number, required: true},
-    description: {type: String, required: true},
+    description: { type: String, required: true },
     category: { type: String, required: true },
-    image: {}
+    image: { type: String, required: true}
+    
 });
 
 export const ProductModel = model<Product>('Product',productSchema)
--- src/shared/models/product.model.ts
+++ src/shared/models/product.model.ts
@@ -1,11 +1,11 @@
 
 import * as mongoose from 'mongoose';
-import { Url } from 'url';
+
 export interface Product {
     _id?:{type: mongoose.Types.ObjectId}
     title: string,
     price: number,
     description: string,
     category: string,
-    image:Url,
+    image:string,
 }
