edf1f509426bb31a394a1f477edda677e720ac9c
mekdes45 <mekmis2020@gmail.com>
2021-11-01 18:03:04 -0700

    feat(model,schema):add products schemas

--- src/server/schemas/product.schema.ts
+++ src/server/schemas/product.schema.ts
@@ -4,12 +4,11 @@ import { Product } from '../../shared/models/product.model.js';
 const {Schema, model} = mongoose
 
 const productSchema = new Schema<Product>({
-    _id: {type: String, required: true},
+   
     title: {type: String, required: true},
     price: {type: Number, required: true},
     description: { type: String, required: true },
-    category: { type: String, required: true },
-    image: { type: String, required: true}
+    imageurl: { type: String, required: true}
     
 });
 
--- src/shared/models/product.model.ts
+++ src/shared/models/product.model.ts
@@ -6,6 +6,6 @@ export interface Product {
     title: string,
     price: number,
     description: string,
-    category: string,
-    image:string,
+ 
+    imageurl:string,
 }
