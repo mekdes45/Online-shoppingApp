5756936a05ae04d84c7c135225384b3262e5a184
mekdes45 <mekmis2020@gmail.com>
2021-11-14 07:43:54 -0800

    feat(cartSchema): add product total price  feature

--- src/server/schemas/cart.schema.ts
+++ src/server/schemas/cart.schema.ts
@@ -1,5 +1,6 @@
 
 import mongoose from 'mongoose';
+import { Product } from '../../shared/models/product.model.js';
 import { Cart } from './../../shared/models/cart.model.js';
 
 const {Schema, model} = mongoose
@@ -7,9 +8,16 @@ const {Schema, model} = mongoose
 const cartSchema = new mongoose.Schema<Cart>({
     user: { type: mongoose.Types.ObjectId, ref: 'User' },
     items: [{ type: mongoose.Types.ObjectId, ref:'Product'}],
-    total:{type:Number,default:0}
    
     
 });
+cartSchema.virtual('total').get(function (this: Cart) {
+    return this.items.reduce((amount: number, item: Product) => {
+        return item.price + amount
+    },0)
+})
+cartSchema.set(`toObject`, {virtuals:true});
+cartSchema.set(`toJSON`, {virtuals:true});
+
 
 export const CartModel = model<Cart>('Cart',cartSchema)
