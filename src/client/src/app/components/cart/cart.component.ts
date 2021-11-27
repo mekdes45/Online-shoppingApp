26a5cfb7cb4d903eaedfa1c81409c868caf22093
mekdes45 <mekmis2020@gmail.com>
2021-11-26 19:24:09 -0800

    fatc(comp):add remove cart functionality

--- src/client/src/app/components/cart/cart.component.ts
+++ src/client/src/app/components/cart/cart.component.ts
@@ -1,4 +1,9 @@
-import { deleteCart, loadcart,  removeFromCart, updateCart } from './../../store/actions/cart/cart.actions';
+import {
+  deleteCart,
+  loadcart,
+  removeFromCart,
+  updateCart,
+} from './../../store/actions/cart/cart.actions';
 import { CartService } from './../../services/cart.service';
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
@@ -7,38 +12,42 @@ import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store';
 import { cartSelector } from 'src/app/store/selectors/cart/cart.selectors';
 import { Product } from '../../../../../shared/models/product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-  cart$: Observable<Cart | null>
- 
-  constructor(private cartService: CartService, private store: Store<AppState>) {
-    this.cart$ = this.store.select(cartSelector)
+  cart$: Observable<Cart | null>;
+
+  constructor(
+    private cartService: CartService,
+    private router: Router,
+    private store: Store<AppState>
+  ) {
+    this.cart$ = this.store.select(cartSelector);
   }
 
   ngOnInit(): void {
-    this.store.dispatch(loadcart())
-    
+    this.store.dispatch(loadcart());
   }
-  deleteProductfromCart(product: Product,) {
-    this.store.dispatch(deleteCart({ data: product }))
-    
+  deleteProductfromCart(product: Product) {
+    this.store.dispatch(deleteCart({ data: product }));
   }
   emptycart() {
-    this.cartService.removeAllCart()
+    this.cartService.removeAllCart();
   }
- 
+
   dec(product: Product) {
-    this.store.dispatch(removeFromCart({ data: product }))
-    
-   
+    this.store.dispatch(removeFromCart({ data: product }));
   }
   inc(product: Product) {
-    this.store.dispatch(updateCart({ data: product }))
+    this.store.dispatch(updateCart({ data: product }));
   }
+  checkout() {
+    this.router.navigate(['/checkout']);
+  }
+ 
 }
-
